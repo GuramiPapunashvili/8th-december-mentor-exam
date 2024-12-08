dd966b85f7402abc25fb32ec9f2df4f58b414d67
GuramiPapunashvili <157598807+GuramiPapunashvili@users.noreply.github.com>
2024-12-08 13:08:05 +0400

    commit

--- Task 7/python.py
+++ Task 7/python.py
@@ -1,12 +1,12 @@
 def primeInRange(num1,num2):
-    res = [] #create an array where i keep result
-    def checkPrime(num): #we create a function for checking a prime
+    res = [] #create an array where i keep resul
+    def checkPrime(num): #we create a function for checkin a prime
         for i in range(2,num): #iterate through every number between 2 and our number
-            if num2 % i == 0: #since we're not taking 1 and the number itself into account it shouldnt have any divisors, so if it does we instantly return False
-                return False 
-        return True #if it doesnt have any divisors we return true
-    for i in range(num1,num2): #now we go through the range given
-        if i == 1: #since 1 is not considered a prime but will pass the requirments we single it out using if
+            if num2 % i == 0: #since were not taking 1 and the number itself into account it shouldnt have any divisors, so if it does we instantly return False
+                return False #return false 
+        return True #if it doesnt have ay divisors we return true
+    for i in range(num1,num2): #no we go through the range given
+        if i == 1: #since 1 is not considered a prime but will pass the requirments we single it ut using if
             continue
         elif checkPrime(i): #otherwise if a number is a prime we append it to the result
             res.append(i)
